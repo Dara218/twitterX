46c70770a7bf9f38d3456da886385a28e98aa43f
Ralph Bautista <ralph.breis.bautista@gmail.com>
2023-08-11 21:39:05 +0800

    Created registerpage2 for 2nd modal of regiseter

---
+++ resources/js/vue-files/login-register/register/RegisterPage2.vue
@@ -0,0 +1,12 @@
+<template>
+    <form @submit.prevent="toRegisterPage2" class="w-full flex flex-col gap-4 px-8">
+        <p class="font-bold text-3xl mt-14 mb-4">Create your account</p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque cumque officia dolore ipsam natus cum saepe ratione recusandae! Sint eum vero quidem quis ipsum mollitia aut doloribus ad veritatis minus!
+
+        <input type="submit" value="Next" class="rounded-full bg-white cursor-pointer py-3 text-black my-8">
+    </form>
+</template>
+
+<script setup>
+
+</script>
