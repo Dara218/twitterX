dcc04f17e8c6ac050ccde48caaeaf55d452b9c8a
Ralph Bautista <ralph.breis.bautista@gmail.com>
2023-08-14 16:34:54 +0800

    Created file for sending of confirmation code to gmail

---
+++ app/Mail/RegisterConfirmationCode.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class RegisterConfirmationCode extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     */
+
+    public $confirmationCode;
+
+    public function __construct($confirmationCode)
+    {
+        $this->confirmationCode = $confirmationCode;
+    }
+
+    public function build()
+    {
+        return $this->from('ralphzacbautista@gmail.com', 'TwitterX')
+                    ->subject('TwitterX Registration code')
+                    ->view('email.emailRegistration')
+                    ->with('confirmationCode', $this->confirmationCode);
+    }
+}
