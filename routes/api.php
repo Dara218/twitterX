716cc3818d564833a259a51c8eb780a6d67915a6
Ralph Bautista <ralph.breis.bautista@gmail.com>
2023-08-14 16:43:03 +0800

    Added route for registerUser

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\DateController;
+use App\Http\Controllers\RegisterUserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -19,3 +20,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 Route::get('/getMonths', [DateController::class, 'getMonths'])->name('getMonths');
+Route::post('/register-user', [RegisterUserController::class, 'store'])->name('registerUser');
