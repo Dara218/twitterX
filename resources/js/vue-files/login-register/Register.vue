10ad77610e8aac90e97f00de6a3238bd0190f90d
Ralph Bautista <ralph.breis.bautista@gmail.com>
2023-08-30 19:29:17 +0800

    Refractor code, added showRegisterModal for dynamic components

--- resources/js/vue-files/login-register/Register.vue
+++ resources/js/vue-files/login-register/Register.vue
@@ -8,24 +8,50 @@
             <div v-if="currentRegisterPageNumber >= 2 ">
                 <i @click="previousRegisterPage" class="fa-solid fa-arrow-left  cursor-pointer"></i>
             </div>
-
             <p v-if="currentRegisterPageNumber < 5" class="text-xl font-bold">Step {{ currentRegisterPageNumber  }} of {{ registerPageNumber  }}</p>
         </div>
 
         <div v-show="currentRegisterPageNumber == 1">
-            <RegisterPage1 :currentRegisterPageNumber="currentRegisterPageNumber" :registerPageNumber="registerPageNumber" @toRegisterPage2="registerNextPage" @details="getUserDetails"/>
+            <RegisterPage1
+                :currentRegisterPageNumber="currentRegisterPageNumber"
+                :registerPageNumber="registerPageNumber"
+                @toRegisterPage2="registerNextPage"
+                @details="getUserDetails"/>
         </div>
-        <div v-if="userDetails != null && currentRegisterPageNumber == 2" v-show="currentRegisterPageNumber == 2">
-            <RegisterPage2 :currentRegisterPageNumber="currentRegisterPageNumber" :registerPageNumber="registerPageNumber" :details="userDetails" @userDetails="getUserDetails" @toRegisterPage3="registerNextPage"/>
+        <div v-if="showRegisterModal(2)" v-show="currentRegisterPageNumber == 2">
+            <RegisterPage2
+                :currentRegisterPageNumber="currentRegisterPageNumber"
+                :registerPageNumber="registerPageNumber"
+                :details="userDetails"
+                @userDetails="getUserDetails"
+                @toRegisterPage3="registerNextPage"/>
         </div>
-        <div v-if="userDetails != null && currentRegisterPageNumber == 3" v-show="currentRegisterPageNumber == 3">
-            <RegisterPage3 :currentRegisterPageNumber="currentRegisterPageNumber" :registerPageNumber="registerPageNumber" :details="userDetails" @toRegisterPage4="registerNextPage"/>
+        <div v-if="showRegisterModal(3)" v-show="currentRegisterPageNumber == 3">
+            <RegisterPage3
+                :currentRegisterPageNumber="currentRegisterPageNumber"
+                :registerPageNumber="registerPageNumber"
+                :details="userDetails"
+                @toRegisterPage4="registerNextPage"/>
         </div>
-        <div v-if="userDetails != null && currentRegisterPageNumber == 4" v-show="currentRegisterPageNumber == 4">
-            <RegisterPage4 :currentRegisterPageNumber="currentRegisterPageNumber" :registerPageNumber="registerPageNumber" :details="userDetails" @toAddProfilePic="registerNextPage"/>
+        <div v-if="showRegisterModal(4)" v-show="currentRegisterPageNumber == 4">
+            <RegisterPage4
+                :currentRegisterPageNumber="currentRegisterPageNumber"
+                :registerPageNumber="registerPageNumber"
+                :details="userDetails"
+                @toAddProfilePic="registerNextPage" @password="storePassword"/>
         </div>
-        <div v-if="userDetails != null && currentRegisterPageNumber == 5" v-show="currentRegisterPageNumber == 5">
-            <RegisterPageProfilePicture :currentRegisterPageNumber="currentRegisterPageNumber" :registerPageNumber="registerPageNumber" :details="userDetails" @toAddProfilePic="registerNextPage"/>
+        <div v-if="showRegisterModal(5)" v-show="currentRegisterPageNumber == 5">
+            <RegisterPageProfilePicture
+                :currentRegisterPageNumber="currentRegisterPageNumber"
+                :registerPageNumber="registerPageNumber"
+                :details="userDetails"
+                @toAddUsername="registerNextPage"/>
+        </div>
+        <div v-if="showRegisterModal(6)" v-show="currentRegisterPageNumber == 6">
+            <RegisterPageAddUsername
+                :currentRegisterPageNumber="currentRegisterPageNumber"
+                :registerPageNumber="registerPageNumber" :password="password"
+                :details="userDetails" />
         </div>
     </div>
 </template>
@@ -38,10 +64,12 @@
     import RegisterPage3 from '../login-register/register/RegisterPage3.vue'
     import RegisterPage4 from '../login-register/register/RegisterPage4.vue'
     import RegisterPageProfilePicture from '../login-register/register/RegisterPageProfilePicture.vue'
+    import RegisterPageAddUsername from '../login-register/register/RegisterPageAddUsername.vue'
 
     let registerPageNumber = ref(4)
     let currentRegisterPageNumber = ref(1)
     let userDetails = ref(null)
+    let password = ref('')
 
     const props = defineProps(['openRegistrationModal'])
 
@@ -51,4 +79,14 @@
     const getUserDetails = (details) => userDetails.value = details
     const previousRegisterPage = () => currentRegisterPageNumber.value--
 
+    const showRegisterModal = number =>
+    {
+        if(userDetails.value != null && currentRegisterPageNumber.value == number)
+        {
+            return true
+        }
+        return false
+    }
+
+    const storePassword = pass => password.value = pass
 </script>
