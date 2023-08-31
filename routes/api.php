da6929fc8338a608928fb62eb055feccfe2c4891
Ralph Bautista <ralph.breis.bautista@gmail.com>
2023-08-31 21:24:14 +0800

    -Created route group for register -Added route for user login

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\DateController;
 use App\Http\Controllers\RegisterUserController;
+use App\Http\Controllers\SessionController;
 use App\Http\Controllers\UserPhotoController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -20,9 +21,15 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
 Route::get('/getMonths', [DateController::class, 'getMonths'])->name('getMonths');
-Route::post('/register-user', [RegisterUserController::class, 'store'])->name('registerUser');
-Route::put('/check-email-verification/{id}', [RegisterUserController::class, 'verifyEmailCode'])->name('verifyEmailCode');
-Route::put('/store-user-password/{id}', [RegisterUserController::class, 'storePassword'])->name('storePassword');
-Route::put('/update-user-photo/{id}', [UserPhotoController::class, 'updateUserPhoto'])->name('updateUserPhoto');
-Route::put('/store-username/{id}', [RegisterUserController::class, 'updateUsername'])->name('updateUsername');
+
+Route::name('register.')->group(function(){
+    Route::post('/register-user', [RegisterUserController::class, 'store'])->name('registerUser');
+    Route::put('/check-email-verification/{id}', [RegisterUserController::class, 'verifyEmailCode'])->name('verifyEmailCode');
+    Route::put('/store-user-password/{id}', [RegisterUserController::class, 'storePassword'])->name('storePassword');
+    Route::put('/update-user-photo/{id}', [UserPhotoController::class, 'updateUserPhoto'])->name('updateUserPhoto');
+    Route::put('/store-username/{id}', [RegisterUserController::class, 'updateUsername'])->name('updateUsername');
+});
+
+Route::post('/login-user', [SessionController::class, 'store'])->name('store');
