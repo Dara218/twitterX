3af7a596da9291cff27f3083a6d9b0d5ad823a4c
Ralph Bautista <ralph.breis.bautista@gmail.com>
2023-08-31 21:18:56 +0800

    Fixed login function of user

--- app/Http/Controllers/SessionController.php
+++ app/Http/Controllers/SessionController.php
@@ -2,9 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\RequestSession;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class SessionController extends Controller
 {
-    //
+    public function store(RequestSession $request)
+    {
+        $request->validated();
+
+        $user = User::where('email', $request->login)
+                    ->orWhere('username', $request->login)
+                    ->first();
+
+        if(! $user || ! Hash::check($request->password, $user->password)){
+            return response()->json(['success' => false]);
+        }
+
+        session()->regenerate();
+        return response()->json(['success' => true]);
+    }
 }
