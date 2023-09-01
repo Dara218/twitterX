121c26e859b799908bd1a496c4c2bed4235d564b
Ralph Bautista <ralph.breis.bautista@gmail.com>
2023-09-01 19:09:18 +0800

    Create a new component for that holds user tweets

---
+++ resources/js/components/HomeTweets.vue
@@ -0,0 +1,156 @@
+<template>
+    <div class="flex flex-col">
+        <div class="flex gap-2 border border-t-0 border-l-0 border-r-0 border-slate-700 p-4">
+            <img src="storage/images/logo-white.png" alt="" class="h-8 rounded-full">
+            <div class="flex flex-col">
+                <div class="flex justify-between gap-2">
+                    <div class="flex gap-2">
+                        <p>Ralph</p>
+                        <p>@ralph218</p>
+                        <p>15h</p>
+                    </div>
+                    <span class="material-symbols-outlined cursor-pointer">
+                        more_horiz
+                    </span>
+                </div>
+                <div>
+                    <div>
+                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Atque itaque doloribus ad rerum voluptatibus magni architecto totam, enim assumenda animi et possimus dolores culpa id similique qui libero facilis ex!
+                    </div>
+                    <div class="full flex justify-around mt-4">
+                        <span :class="tweetOptionClass">
+                            chat_bubble
+                        </span>
+                        <span :class="tweetOptionClass">
+                            quick_phrases
+                        </span>
+                        <span :class="tweetOptionClass">
+                            favorite
+                        </span>
+                        <span :class="tweetOptionClass">
+                            bar_chart
+                        </span>
+                        <span :class="tweetOptionClass">
+                            ios_share
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="flex gap-2 border border-t-0 border-l-0 border-r-0 border-slate-700 p-4">
+            <img src="storage/images/logo-white.png" alt="" class="h-8 rounded-full">
+            <div class="flex flex-col">
+                <div class="flex justify-between gap-2">
+                    <div class="flex gap-2">
+                        <p>Ralph</p>
+                        <p>@ralph218</p>
+                        <p>15h</p>
+                    </div>
+                    <span class="material-symbols-outlined cursor-pointer">
+                        more_horiz
+                    </span>
+                </div>
+                <div>
+                    <div>
+                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Atque itaque doloribus ad rerum voluptatibus magni architecto totam, enim assumenda animi et possimus dolores culpa id similique qui libero facilis ex!
+                    </div>
+                    <div class="full flex justify-around mt-4">
+                        <span :class="tweetOptionClass">
+                            chat_bubble
+                        </span>
+                        <span :class="tweetOptionClass">
+                            quick_phrases
+                        </span>
+                        <span :class="tweetOptionClass">
+                            favorite
+                        </span>
+                        <span :class="tweetOptionClass">
+                            bar_chart
+                        </span>
+                        <span :class="tweetOptionClass">
+                            ios_share
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="flex gap-2 border border-t-0 border-l-0 border-r-0 border-slate-700 p-4">
+            <img src="storage/images/logo-white.png" alt="" class="h-8 rounded-full">
+            <div class="flex flex-col">
+                <div class="flex justify-between gap-2">
+                    <div class="flex gap-2">
+                        <p>Ralph</p>
+                        <p>@ralph218</p>
+                        <p>15h</p>
+                    </div>
+                    <span class="material-symbols-outlined cursor-pointer">
+                        more_horiz
+                    </span>
+                </div>
+                <div>
+                    <div>
+                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Atque itaque doloribus ad rerum voluptatibus magni architecto totam, enim assumenda animi et possimus dolores culpa id similique qui libero facilis ex!
+                    </div>
+                    <div class="full flex justify-around mt-4">
+                        <span :class="tweetOptionClass">
+                            chat_bubble
+                        </span>
+                        <span :class="tweetOptionClass">
+                            quick_phrases
+                        </span>
+                        <span :class="tweetOptionClass">
+                            favorite
+                        </span>
+                        <span :class="tweetOptionClass">
+                            bar_chart
+                        </span>
+                        <span :class="tweetOptionClass">
+                            ios_share
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="flex gap-2 border border-t-0 border-l-0 border-r-0 border-slate-700 p-4">
+            <img src="storage/images/logo-white.png" alt="" class="h-8 rounded-full">
+            <div class="flex flex-col">
+                <div class="flex justify-between gap-2">
+                    <div class="flex gap-2">
+                        <p>Ralph</p>
+                        <p>@ralph218</p>
+                        <p>15h</p>
+                    </div>
+                    <span class="material-symbols-outlined cursor-pointer">
+                        more_horiz
+                    </span>
+                </div>
+                <div>
+                    <div>
+                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Atque itaque doloribus ad rerum voluptatibus magni architecto totam, enim assumenda animi et possimus dolores culpa id similique qui libero facilis ex!
+                    </div>
+                    <div class="full flex justify-around mt-4">
+                        <span :class="tweetOptionClass">
+                            chat_bubble
+                        </span>
+                        <span :class="tweetOptionClass">
+                            quick_phrases
+                        </span>
+                        <span :class="tweetOptionClass">
+                            favorite
+                        </span>
+                        <span :class="tweetOptionClass">
+                            bar_chart
+                        </span>
+                        <span :class="tweetOptionClass">
+                            ios_share
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+    const tweetOptionClass = 'material-symbols-outlined cursor-pointer'
+</script>
