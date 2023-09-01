a03990c6ba93274206cfd1440c35f9bfb3ac9953
Ralph Bautista <ralph.breis.bautista@gmail.com>
2023-09-01 19:14:28 +0800

    Fixed right bar

--- resources/js/vue-files/home/Home.vue
+++ resources/js/vue-files/home/Home.vue
@@ -1,25 +1,37 @@
 <template>
-    <div class="flex">
-        <Sidebar/>
-        <div class="flex flex-1 text-white">
-            <div class="fixed top-0 w-1/2 bg-black">
-                <div class="cursor-pointer text-xl py-4">Home</div>
-                <div class="grid grid-cols-2">
-                    <div :class="middleButtonClass">
-                        <div class="border-b-2 border-blue-500">For you</div>
+    <div class="px-40 md:px-0">
+        <div class="grid grid-cols-8">
+            <Sidebar class="col-span-2 sticky top-0"/>
+            <div class="grid grid-cols-5 col-span-6 text-white">
+                <div class="relative col-span-3 bg-black">
+                    <div class="sticky top-0 w-full bg-black bg-opacity-50 backdrop-blur-md border border-slate-700">
+                        <div class="cursor-pointer text-xl py-4 ml-4">Home</div>
+                        <div class="grid grid-cols-2">
+                            <div :class="middleButtonClass">
+                                <div class="border-b-2 border-blue-500">For you</div>
+                            </div>
+                            <div :class="middleButtonClass">Following</div>
+                        </div>
                     </div>
-                    <div :class="middleButtonClass">Following</div>
+                    <div class="bg-black text-white min-h-screen border border-slate-700">
+                        <HomeTweetsVue/>
+                    </div>
+                </div>
+                <div class="flex flex-col col-span-2 gap-6 bg-black px-8 py-4">
+                    <div class="bg-black sticky top-0 pb-2">
+                        <input type="search" name="search" id="search" class="bg-gray-800 text-white rounded-full py-2 px-2 w-full" placeholder="Search">
+                    </div>
+                    <RightBar class="col-span-2"/>
                 </div>
             </div>
-            <div class="mt-32 mx-4 flex-1 text-black">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio dolores aliquid ab? Natus quisquam ut maxime tempore velit commodi consequuntur rerum dicta atque? Nam quod odio rerum minima provident optio!
-            </div>
         </div>
-        <Sidebar/>
     </div>
 </template>
 
 <script setup>
-    import Sidebar from '../../components/Sidebar.vue';
+    import Sidebar from '../../components/Sidebar.vue'
+    import RightBar from '../../components/RightBar.vue'
+    import HomeTweetsVue from '../../components/HomeTweets.vue'
+
     const middleButtonClass = 'col-span-1 cursor-pointer flex hover:bg-slate-800 justify-center py-4 text-center'
 </script>
