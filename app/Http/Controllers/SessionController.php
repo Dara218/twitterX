51d972f81760c6bb1443f1514abfad1f823eace9
Ralph Bautista <ralph.breis.bautista@gmail.com>
2023-09-04 19:44:44 +0800

    -Refactor login functionality to use Laravel's built-in Auth::attempt -Added logout function

--- app/Http/Controllers/SessionController.php
+++ app/Http/Controllers/SessionController.php
@@ -3,10 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\RequestSession;
-use App\Models\User;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Hash;
 
 class SessionController extends Controller
 {
@@ -14,15 +11,18 @@ class SessionController extends Controller
     {
         $request->validated();
 
-        $user = User::where('email', $request->login)
-                    ->orWhere('username', $request->login)
-                    ->first();
-
-        if(! $user || ! Hash::check($request->password, $user->password)){
+        if (!Auth::attempt(['email' => $request->login, 'password' => $request->password])
+            && !Auth::attempt(['username' => $request->login, 'password' => $request->password]))
+        {
             return response()->json(['success' => false]);
         }
 
         session()->regenerate();
         return response()->json(['success' => true]);
+    }
+
+    public function logout()
+    {
+        Auth::logout();
     }
 }
