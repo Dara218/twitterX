00b94f02a5ef76e52bc6928debfc824dbb65727c
Ralph Bautista <ralph.breis.bautista@gmail.com>
2023-09-05 19:52:54 +0800

    style: Add text-slate-400 class to password visibility icon
    This commit adds the `text-slate-400` class to the password visibility icon, ensuring that the icon's color is consistent with the design. The change improves the visual appearance of the element.
    
    No functional changes were made

--- resources/js/vue-files/login-register/login/LoginModalPassword.vue
+++ resources/js/vue-files/login-register/login/LoginModalPassword.vue
@@ -4,7 +4,7 @@
         <input v-model="userInput" disabled type="text" name="login" id="login" class="w-full bg-gray-800 border-none text-slate-400 p-3 border border-slate-400">
 
         <div class="relative">
-            <span @click="togglePassword" class="material-symbols-outlined cursor-pointer absolute right-2 top-1/2 transform -translate-y-1/2">visibility</span>
+            <span @click="togglePassword" class="material-symbols-outlined cursor-pointer absolute right-2 top-1/2 transform -translate-y-1/2 text-slate-400">visibility</span>
 
             <input v-model="password" :type="inputType" name="password" id="password" placeholder="Password" class="w-full bg-transparent text-white p-3 border border-slate-400">
         </div>
